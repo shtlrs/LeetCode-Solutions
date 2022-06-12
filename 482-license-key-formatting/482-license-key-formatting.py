7a3def95fb0240467f3b3bb02705577980dad637
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-06-12 13:50:27 +0100

    Time: 110 ms (32.88%), Space: 15 MB (28.74%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.py
+++ 482-license-key-formatting/482-license-key-formatting.py
@@ -2,16 +2,20 @@ class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
         dash = "-"
         n = 0
+        
         for char in s:
             if char != dash:
                 n += 1
+                
         s = "".join(s.upper().strip().split(dash))
         first_sub_string_len = n % k
         new_s = []
+        
         if first_sub_string_len:
             new_s = [s[: first_sub_string_len]]
         s = s[first_sub_string_len: ]
         start = 0
+        
         for ix, char in enumerate(s):
             if ((ix + 1) % k ) == 0:
                 new_s.append(s[start: ix + 1])
