eb596397227a617c5675597e18204d5ace1a8b67
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-06-12 13:25:28 +0100

    Time: 214 ms (16.23%), Space: 15.2 MB (71.01%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        result = []
         distinct_elements = set(nums)
         return [element for element in distinct_elements if nums.count(element) > n/3]
