de3f007074a67cc90e0ec53364835c486c7ceff5
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-06-12 13:37:15 +0100

    Time: 58 ms (14.34%), Space: 13.9 MB (61.47%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        shift_number= 0
+        while shift_number<len(s):
+            s= s[1:]+s[0]
+            if s == goal:
+                return True
+            shift_number+=1
+        return False
